b555c906a7a80b19db8eb556705f23c86c084e1c
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-08-07 13:57:08 +0200

    Add AfterLogin Page

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:fintess_prodigy/app/after%20login/after_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -6,37 +7,21 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RootPage();
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
-          if (user == null) {
-            return const Scaffold(
-              body: Center(
-                child: Text(
-                  'Jesteś niezalogowany',
-                ),
-              ),
-            );
-          }
-          return Scaffold(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+        if (user == null) {
+          return const Scaffold(
             body: Center(
               child: Text(
-                'Jesteś zalogowany jako ${user.email}',
+                'Jesteś niezalogowany',
               ),
             ),
           );
-        });
+        }
+        return AfterLogin(user: user);
+      },
+    );
   }
 }
