4f4af0ff87c2262ef334ae0010ba6fe1a668c428
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-08-09 18:46:21 +0200

    Custiomize ExerciseExample Page with logout option

--- lib/app/after login plus features/exercies_examples/exercies_examples_page.dart
+++ lib/app/after login plus features/exercies_examples/exercies_examples_page.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_prodigy/app/before%20login/main_login_page.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseExamplesPage extends StatelessWidget {
+class ExerciseExamplesPage extends StatefulWidget {
   const ExerciseExamplesPage({
     super.key,
   });
+
+  @override
+  State<ExerciseExamplesPage> createState() => _ExerciseExamplesPageState();
+}
+
+class _ExerciseExamplesPageState extends State<ExerciseExamplesPage> {
+  var currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +21,41 @@ class ExerciseExamplesPage extends StatelessWidget {
         title: const Text(
           'Exercise Examples',
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+          if (newIndex == 3) {
+            FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => MainLoginPage()),
+            );
+          }
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Workouts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
+          ),
+        ],
       ),
     );
   }
