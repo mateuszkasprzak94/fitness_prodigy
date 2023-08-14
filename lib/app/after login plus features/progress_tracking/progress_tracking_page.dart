d94842558c53de44b4208afc1f858043644620d6
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-08-14 15:25:54 +0200

    Add BottomNawBar to Progress Tracking Page

--- lib/app/after login plus features/progress_tracking/progress_tracking_page.dart
+++ lib/app/after login plus features/progress_tracking/progress_tracking_page.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/exercies_examples/exercies_examples_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/features_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/user_profile/user_profile_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/workout_plans/workout_plans_page.dart';
 import 'package:flutter/material.dart';
 
-class ProgressTrackingPage extends StatelessWidget {
+class ProgressTrackingPage extends StatefulWidget {
   const ProgressTrackingPage({
-    super.key,
-  });
+    Key? key,
+    required this.user,
+  }) : super(key: key);
 
+  final User user;
+
+  @override
+  State<ProgressTrackingPage> createState() => _ProgressTrackingPageState();
+}
+
+class _ProgressTrackingPageState extends State<ProgressTrackingPage> {
+  var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +26,69 @@ class ProgressTrackingPage extends StatelessWidget {
         title: const Text(
           'Progress Tracking',
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+          if (newIndex == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => Features(
+                  user: widget.user,
+                ),
+              ),
+            );
+          } else if (newIndex == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ExerciseExamplesPage(
+                  user: widget.user,
+                ),
+              ),
+            );
+          } else if (newIndex == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => WorkoutPlansPage(
+                  user: widget.user,
+                ),
+              ),
+            );
+          } else if (newIndex == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => UserProfilePage(user: widget.user),
+              ),
+            );
+          }
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Workouts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'User Profile',
+          ),
+        ],
       ),
     );
   }
