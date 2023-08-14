74cace915cf60b953ff0cf146221efced243c524
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-08-14 15:27:30 +0200

    Add BottomNawBar to Social Sharing Page

--- lib/app/after login plus features/social_sharing/social_sharing_button.dart
+++ lib/app/after login plus features/social_sharing/social_sharing_button.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/social_sharing/social_sharing_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,8 @@ class SocialSharing extends StatelessWidget {
       onPressed: () {
         Navigator.of(context).push(
           MaterialPageRoute(
-            builder: (_) => const SocialSharingPage(),
+            builder: (_) =>
+                SocialSharingPage(user: FirebaseAuth.instance.currentUser!),
           ),
         );
       },
--- lib/app/after login plus features/social_sharing/social_sharing_page.dart
+++ lib/app/after login plus features/social_sharing/social_sharing_page.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/exercies_examples/exercies_examples_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/user_profile/user_profile_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/workout_plans/workout_plans_page.dart';
 import 'package:flutter/material.dart';
 
-class SocialSharingPage extends StatelessWidget {
+class SocialSharingPage extends StatefulWidget {
   const SocialSharingPage({
-    super.key,
-  });
+    Key? key,
+    required this.user,
+  }) : super(key: key);
 
+  final User user;
+
+  @override
+  State<SocialSharingPage> createState() => _SocialSharingPageState();
+}
+
+class _SocialSharingPageState extends State<SocialSharingPage> {
+  var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +25,56 @@ class SocialSharingPage extends StatelessWidget {
         title: const Text(
           'Social Sharing',
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+          if (newIndex == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ExerciseExamplesPage(user: widget.user),
+              ),
+            );
+          } else if (newIndex == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => WorkoutPlansPage(user: widget.user),
+              ),
+            );
+          } else if (newIndex == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => UserProfilePage(user: widget.user),
+              ),
+            );
+          }
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Workouts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: 'User Profile',
+          ),
+        ],
       ),
     );
   }
