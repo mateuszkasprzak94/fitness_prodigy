cc83a72ecfe10f3e6f47e4d830adb0f31aa4e452
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-08-20 13:57:09 +0200

    Add text to advanced workout

--- lib/app/after login plus features/workout_plans/workout_plans_page.dart
+++ lib/app/after login plus features/workout_plans/workout_plans_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/exercies_examples/exercies_examples_page.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/features_page.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/user_profile/user_profile_page.dart';
+import 'package:fitness_prodigy/app/after%20login%20plus%20features/workout_plans/advanced_workout.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/workout_plans/beginner_workout.dart';
 import 'package:fitness_prodigy/app/after%20login%20plus%20features/workout_plans/intermediate_workout.dart';
 import 'package:flutter/material.dart';
@@ -57,7 +58,14 @@ class _WorkoutPlansPageState extends State<WorkoutPlansPage> {
           ),
           const SizedBox(height: 15),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) =>
+                      AdvancedWorkout(user: FirebaseAuth.instance.currentUser!),
+                ),
+              );
+            },
             child: const Text('Advanced Workout'),
           ),
           const SizedBox(height: 15),
