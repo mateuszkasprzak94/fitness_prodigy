ba126c1764516683bd2b4a810e862a33089a2e8e
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-06 22:33:45 +0200

    Transfer all buttons widget back to workout plan

--- lib/app/features/workout_plans/workout_plans_page.dart
+++ lib/app/features/workout_plans/workout_plans_page.dart
@@ -2,17 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_prodigy/app/features/exercies_examples/exercies_examples_page.dart';
 import 'package:fitness_prodigy/app/features/features_page.dart';
 import 'package:fitness_prodigy/app/features/user_profile/user_profile_page.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/abs_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/advanced_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/back_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/beginner_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/biceps_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/chest_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/forearms_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/intermediate_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/legs_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/shoulders_workout_button.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_buttons/triceps_workout_button.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/abs_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/advanced_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/back_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/beginner_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/biceps_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/chest_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/forearms_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/intermediate_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/legs_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/shoulders_workout.dart';
+import 'package:fitness_prodigy/app/features/workout_plans/triceps_workout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -131,6 +131,523 @@ class _WorkoutPlansPageState extends State<WorkoutPlansPage> {
             label: 'User Profile',
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BeginnerWorkoutButton extends StatelessWidget {
+  const BeginnerWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    BeginnerWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Beginner Workout',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IntermediateWorkoutButton extends StatelessWidget {
+  const IntermediateWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => IntermediateWorkout(
+                    user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Intermediate Workout',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AdvancedWorkoutButton extends StatelessWidget {
+  const AdvancedWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    AdvancedWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Advanced Workout',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChestWorkoutButton extends StatelessWidget {
+  const ChestWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    ChestWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Chest',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BackWorkoutButton extends StatelessWidget {
+  const BackWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    BackWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Back',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShouldersWorkoutButton extends StatelessWidget {
+  const ShouldersWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    ShouldersWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Shoulders',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BicepsWorkoutButton extends StatelessWidget {
+  const BicepsWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    BicepsWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Biceps',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TricepsWorkoutButton extends StatelessWidget {
+  const TricepsWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    TricepsWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Triceps',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForearmsWorkoutButton extends StatelessWidget {
+  const ForearmsWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    ForearmsWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Forearms',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LegsWorkoutButton extends StatelessWidget {
+  const LegsWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    LegsWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Legs',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AbsWorkoutButton extends StatelessWidget {
+  const AbsWorkoutButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const shape = StadiumBorder();
+    return Material(
+      color: Colors.transparent,
+      shape: shape,
+      elevation: 8,
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: shape,
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(164, 0, 0, 0),
+            Colors.white,
+          ], begin: Alignment.centerRight, end: Alignment.centerLeft),
+        ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            //deactivate color and shadow
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) =>
+                    AbsWorkout(user: FirebaseAuth.instance.currentUser!),
+              ),
+            );
+          },
+          child: const Text(
+            'Abs',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
       ),
     );
   }
