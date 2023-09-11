fedf82bdd77f626c82d4737b962bf03d9a422a82
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-11 13:35:16 +0200

    Add transparent app bar to motivation quotes

--- lib/app/features/motivation_quotes/motivations_quotes_page.dart
+++ lib/app/features/motivation_quotes/motivations_quotes_page.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fitness_prodigy/app/features/exercies_examples/exercies_examples_page.dart';
-import 'package:fitness_prodigy/app/features/features_page.dart';
-import 'package:fitness_prodigy/app/features/user_profile/user_profile_page.dart';
-import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_page.dart';
+// import 'package:fitness_prodigy/app/features/exercies_examples/exercies_examples_page.dart';
+// import 'package:fitness_prodigy/app/features/features_page.dart';
+// import 'package:fitness_prodigy/app/features/user_profile/user_profile_page.dart';
+// import 'package:fitness_prodigy/app/features/workout_plans/workout_plans_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,88 +24,91 @@ class _MotivationQuotesPageState extends State<MotivationQuotesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: Text(
           'Motivation Quotes',
-          style: GoogleFonts.satisfy(fontSize: 30, color: Colors.black),
+          style: GoogleFonts.satisfy(fontSize: 30, color: Colors.white),
         ),
         centerTitle: true,
         flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(164, 0, 0, 0),
-              Colors.white,
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-          ),
-        ),
+            // decoration: const BoxDecoration(
+            //   gradient: LinearGradient(colors: [
+            //     Color.fromARGB(164, 0, 0, 0),
+            //     Colors.white,
+            //   ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+            // ),
+            ),
         systemOverlayStyle: SystemUiOverlayStyle.light,
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
+        foregroundColor: Colors.white,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (newIndex) {
-          setState(() {
-            currentIndex = newIndex;
-          });
-          if (newIndex == 0) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => Features(
-                  user: widget.user,
-                ),
-              ),
-            );
-          } else if (newIndex == 1) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => ExerciseExamplesPage(
-                  user: widget.user,
-                ),
-              ),
-            );
-          } else if (newIndex == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => WorkoutPlansPage(
-                  user: widget.user,
-                ),
-              ),
-            );
-          } else if (newIndex == 3) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => UserProfilePage(user: widget.user),
-              ),
-            );
-          }
-        },
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.accessibility),
-            label: 'Exercises',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Workouts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
-            label: 'User Profile',
-          ),
-        ],
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: currentIndex,
+      //   onTap: (newIndex) {
+      //     setState(() {
+      //       currentIndex = newIndex;
+      //     });
+      //     if (newIndex == 0) {
+      //       Navigator.push(
+      //         context,
+      //         MaterialPageRoute(
+      //           builder: (_) => Features(
+      //             user: widget.user,
+      //           ),
+      //         ),
+      //       );
+      //     } else if (newIndex == 1) {
+      //       Navigator.push(
+      //         context,
+      //         MaterialPageRoute(
+      //           builder: (_) => ExerciseExamplesPage(
+      //             user: widget.user,
+      //           ),
+      //         ),
+      //       );
+      //     } else if (newIndex == 2) {
+      //       Navigator.push(
+      //         context,
+      //         MaterialPageRoute(
+      //           builder: (_) => WorkoutPlansPage(
+      //             user: widget.user,
+      //           ),
+      //         ),
+      //       );
+      //     } else if (newIndex == 3) {
+      //       Navigator.push(
+      //         context,
+      //         MaterialPageRoute(
+      //           builder: (_) => UserProfilePage(user: widget.user),
+      //         ),
+      //       );
+      //     }
+      //   },
+      //   selectedItemColor: Colors.black,
+      //   unselectedItemColor: Colors.black,
+      //   showSelectedLabels: true,
+      //   showUnselectedLabels: true,
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.accessibility),
+      //       label: 'Exercises',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.fitness_center),
+      //       label: 'Workouts',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.account_box),
+      //       label: 'User Profile',
+      //     ),
+      //   ],
+      // ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
