52828fc8eb78e66982d703904d058f98e284cee6
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-25 20:37:12 +0200

    Add DecorationImage and text to weather page

--- lib/app/pages/features/weather/weather_page.dart
+++ lib/app/pages/features/weather/weather_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+String description =
+    "Are you planning to take your workout outdoors? Don't let unexpected weather catch you off guard! Check the current conditions in your location to make the most of your fitness routine. Whether it's a sunny day perfect for a run or a chance of rain that calls for an indoor workout, we've got you covered. Stay informed and stay active! 💪⛅️🌧️";
 
 class WeatherContent extends StatefulWidget {
   const WeatherContent({super.key});
@@ -11,10 +15,34 @@ class _WeatherContentState extends State<WeatherContent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Check weather in your location'),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Welcome to the Weather Center! 🌦️',
+          style: GoogleFonts.satisfy(fontSize: 24, color: Colors.black),
+          maxLines: 2,
+        ),
         centerTitle: true,
         automaticallyImplyLeading: true,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/weather4.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Text(
+                description,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
