76543d18814c7c390c782557d569fa54ed112933
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-26 19:39:09 +0200

    Add GestureDetector to Weather Page

--- lib/app/pages/features/weather/weather_page.dart
+++ lib/app/pages/features/weather/weather_page.dart
@@ -19,60 +19,63 @@ class _WeatherContentState extends State<WeatherContent> {
   @override
   Widget build(BuildContext context) {
     cityController.addListener(() => setState(() {}));
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(30),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
+            ),
           ),
-        ),
-        backgroundColor: Colors.black.withOpacity(0.15),
-        title: Text(
-          'Welcome to the Weather Center! 🌦️',
-          style: GoogleFonts.satisfy(fontSize: 24, color: Colors.white),
-          maxLines: 2,
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/weather4.png'),
-            fit: BoxFit.fill,
+          backgroundColor: Colors.black.withOpacity(0.15),
+          title: Text(
+            'Welcome to the Weather Center! 🌦️',
+            style: GoogleFonts.satisfy(fontSize: 24, color: Colors.white),
+            maxLines: 2,
           ),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              Container(
-                color: Colors.black.withOpacity(0.25),
-                child: Text(
-                  description,
-                  style: const TextStyle(
-                    color: Colors.white,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/weather4.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                Container(
+                  color: Colors.black.withOpacity(0.25),
+                  child: Text(
+                    description,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: cityController,
-                decoration: InputDecoration(
-                  labelText: 'City',
-                  hintText: 'City',
-                  prefixIcon: const Icon(Icons.home),
-                  suffixIcon: cityController.text.isEmpty
-                      ? Container(width: 0)
-                      : IconButton(
-                          onPressed: () => cityController.clear(),
-                          icon: const Icon(Icons.close)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15)),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: cityController,
+                  decoration: InputDecoration(
+                    labelText: 'City',
+                    hintText: 'City',
+                    prefixIcon: const Icon(Icons.home),
+                    suffixIcon: cityController.text.isEmpty
+                        ? Container(width: 0)
+                        : IconButton(
+                            onPressed: () => cityController.clear(),
+                            icon: const Icon(Icons.close)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
