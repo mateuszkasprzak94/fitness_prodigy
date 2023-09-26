89285c9fd02e247cd5f6272080a85d8822f1d108
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-26 19:33:57 +0200

    Add GestureDetector to Login Page

--- lib/app/pages/login_page/login_page.dart
+++ lib/app/pages/login_page/login_page.dart
@@ -22,175 +22,158 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     widget.emailController.addListener(() => setState(() {}));
-    return Scaffold(
-      body: BlocProvider(
-        create: (context) => AuthCubit(),
-        child: BlocBuilder<AuthCubit, AuthState>(
-          builder: (context, state) {
-            final errorMessage = state.errorMessage;
-            return Center(
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('images/step3.png'), fit: BoxFit.fill),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 3, 20, 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        isCreatingAccount == true ? 'Register' : 'Login',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: widget.emailController,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.email),
-                          suffixIcon: widget.emailController.text.isEmpty
-                              ? Container(width: 0)
-                              : IconButton(
-                                  onPressed: () =>
-                                      widget.emailController.clear(),
-                                  icon: const Icon(Icons.close)),
-                          hintText: 'E-mail',
-                          filled: true,
-                          fillColor: const Color.fromARGB(69, 255, 255, 255),
-                          hintStyle: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      TextField(
-                        controller: widget.passwordController,
-                        obscureText: isPasswordVisible,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: isPasswordVisible
-                                ? const Icon(Icons.visibility_off)
-                                : const Icon(Icons.visibility),
-                            onPressed: () => setState(
-                                () => isPasswordVisible = !isPasswordVisible),
-                          ),
-                          hintText: 'Password',
-                          filled: true,
-                          fillColor: const Color.fromARGB(69, 255, 255, 255),
-                          hintStyle: const TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        errorMessage,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      // const SizedBox(height: 25),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (isCreatingAccount == true) {
-                            // rejestracja
-                            // try {
-                            context.read<AuthCubit>().register(
-                                widget.emailController.text,
-                                widget.passwordController.text);
-                            // await FirebaseAuth.instance
-                            //     .createUserWithEmailAndPassword(
-                            //   email: widget.emailController.text,
-                            //   password: widget.passwordController.text,
-                            // );
-                            //   } catch (error) {
-                            //     setState(() {
-                            //       errorMessage = error.toString();
-                            //     });
-                            //   }
-                          } else {
-                            //   // logowanie
-                            //   try {
-                            context.read<AuthCubit>().signIn(
-                                widget.emailController.text,
-                                widget.passwordController.text);
-                            // await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            //   email: widget.emailController.text,
-                            //   password: widget.passwordController.text,
-                            // );
-                            // } catch (error) {
-                            //   setState(() {
-                            //     errorMessage = error.toString();
-                            //   });
-                            // }
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white.withOpacity(0.5),
-                        ),
-                        child: Text(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: BlocProvider(
+          create: (context) => AuthCubit(),
+          child: BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+              final errorMessage = state.errorMessage;
+              return Center(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/step3.png'),
+                        fit: BoxFit.fill),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 3, 20, 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
                           isCreatingAccount == true ? 'Register' : 'Login',
                           style: const TextStyle(
                             color: Colors.white,
-                            fontSize: 18,
+                            fontSize: 25,
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 18.0),
-                      if (isCreatingAccount == false) ...[
-                        TextButton(
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: widget.emailController,
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.email),
+                            suffixIcon: widget.emailController.text.isEmpty
+                                ? Container(width: 0)
+                                : IconButton(
+                                    onPressed: () =>
+                                        widget.emailController.clear(),
+                                    icon: const Icon(Icons.close)),
+                            hintText: 'E-mail',
+                            filled: true,
+                            fillColor: const Color.fromARGB(69, 255, 255, 255),
+                            hintStyle: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        TextField(
+                          controller: widget.passwordController,
+                          obscureText: isPasswordVisible,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: isPasswordVisible
+                                  ? const Icon(Icons.visibility_off)
+                                  : const Icon(Icons.visibility),
+                              onPressed: () => setState(
+                                  () => isPasswordVisible = !isPasswordVisible),
+                            ),
+                            hintText: 'Password',
+                            filled: true,
+                            fillColor: const Color.fromARGB(69, 255, 255, 255),
+                            hintStyle: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          errorMessage,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        ElevatedButton(
                           onPressed: () {
-                            setState(() {
-                              isCreatingAccount = true;
-                            });
+                            if (isCreatingAccount == true) {
+                              // rejestracja
+
+                              context.read<AuthCubit>().register(
+                                  widget.emailController.text,
+                                  widget.passwordController.text);
+                            } else {
+                              //   // logowanie
+                              context.read<AuthCubit>().signIn(
+                                  widget.emailController.text,
+                                  widget.passwordController.text);
+                            }
                           },
-                          child: const Text(
-                            'Create Account',
-                            style: TextStyle(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white.withOpacity(0.5),
+                          ),
+                          child: Text(
+                            isCreatingAccount == true ? 'Register' : 'Login',
+                            style: const TextStyle(
                               color: Colors.white,
                               fontSize: 18,
                             ),
                           ),
                         ),
-                      ],
-                      if (isCreatingAccount == true) ...[
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              isCreatingAccount = false;
-                            });
-                          },
-                          child: const Text(
-                            'Have an account?',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
+                        const SizedBox(height: 18.0),
+                        if (isCreatingAccount == false) ...[
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                isCreatingAccount = true;
+                              });
+                            },
+                            child: const Text(
+                              'Create Account',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
                             ),
                           ),
-                        ),
+                        ],
+                        if (isCreatingAccount == true) ...[
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                isCreatingAccount = false;
+                              });
+                            },
+                            child: const Text(
+                              'Have an account?',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
                       ],
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
