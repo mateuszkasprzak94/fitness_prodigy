d5687af13833dd88ed8e3940cca362dc43c5e650
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-09-26 12:53:41 +0200

    Add titleSpacing in Triceps page

--- lib/app/pages/exercies_examples/muscle_anatomy/triceps_page.dart
+++ lib/app/pages/exercies_examples/muscle_anatomy/triceps_page.dart
@@ -28,6 +28,7 @@ class TricepsPage extends StatelessWidget {
           ),
         ),
         systemOverlayStyle: SystemUiOverlayStyle.light,
+        titleSpacing: 0,
       ),
       body: ListView(
         padding: const EdgeInsets.all(16),
