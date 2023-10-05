2be06104a5ca7c2bc34bdf24c53bb64e27a62683
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-10-05 10:42:51 +0200

    Finish creating Vitamin C page content

---
+++ lib/app/pages/features/supplements/supplements_type/vitamins_type/text_for_vitamins/text_for_vitamins.dart
@@ -0,0 +1,41 @@
+// VITAMIN C
+
+String descriptionVitaminC =
+    "Vitamin C, also known as ascorbic acid, is a water-soluble vitamin that plays a crucial role in various bodily functions. It's an essential nutrient, which means your body can't produce it, and you must obtain it from your diet or supplements. Vitamin C is well-known for its powerful antioxidant properties and its role in supporting a healthy immune system.";
+
+String benefits1VitaminC =
+    "Vitamin C is renowned for its ability to boost the immune system. It helps your body produce white blood cells, which are essential for fighting infections and illnesses.";
+
+String benefits2VitaminC =
+    "As an antioxidant, Vitamin C helps protect cells from damage caused by free radicals. This, in turn, may reduce the risk of chronic diseases and slow down the aging process.";
+
+String benefits3VitaminC =
+    "Vitamin C is vital for the synthesis of collagen, a protein that supports skin, connective tissues, and wound healing. It contributes to maintaining healthy skin and may help reduce the appearance of wrinkles.";
+
+String benefits4VitaminC =
+    "Vitamin C enhances the absorption of non-heme iron (the type of iron found in plant-based foods), making it valuable for individuals with iron-deficiency anemia.";
+
+String benefits5VitaminC =
+    "Some research suggests that Vitamin C may lower the risk of heart disease by improving blood vessel function and reducing blood pressure.";
+
+String sourceVitaminC =
+    """You can obtain Vitamin C from various food sources, including:
+
+- Citrus fruits (oranges, lemons, grapefruits)
+- Berries (strawberries, blueberries, raspberries)
+- Kiwi
+- Red and green bell peppers
+- Broccoli
+- Spinach
+- Tomatoes""";
+
+String supplementationVitaminC =
+    "While it's ideal to get your Vitamin C from a balanced diet rich in fruits and vegetables, supplements can be beneficial, especially for individuals with dietary restrictions or specific health needs. Vitamin C supplements are available in various forms, including tablets, capsules, and powders.";
+
+String intakeVitaminC =
+    "The recommended daily intake of Vitamin C varies by age, gender, and life stage. For most adults, a daily dose of 75-90 milligrams is sufficient. However, consult with a healthcare professional before starting any supplement regimen, as individual needs may differ.";
+
+String cautionVitaminC =
+    """While Vitamin C is generally safe when taken within recommended doses, excessive intake can lead to digestive discomfort, including diarrhea. If you're unsure about the right dosage for your needs, consult with a healthcare provider.
+
+Remember that this is a general overview, and individual requirements may vary. It's always a good practice to consult with a healthcare professional before adding any supplements to your routine, especially if you have specific health concerns or medical conditions.""";
--- lib/app/pages/features/supplements/supplements_type/vitamins_type/vitamin_c.dart
+++ lib/app/pages/features/supplements/supplements_type/vitamins_type/vitamin_c.dart
@@ -1,10 +1,117 @@
+import 'package:fitness_prodigy/app/pages/features/supplements/supplements_type/vitamins_type/text_for_vitamins/text_for_vitamins.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class VitaminCContent extends StatelessWidget {
   const VitaminCContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Vitamin C',
+          style: GoogleFonts.lancelot(fontSize: 30, color: Colors.orange),
+        ),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.yellow,
+              Colors.white,
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+          ),
+        ),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+      body: Center(
+          child: ListView(
+        children: [
+          const Image(
+            image: AssetImage('images supplements/vitamin_c.jpg'),
+          ),
+          const SizedBox(height: 15),
+          _buildSupplements1('Overview:', descriptionVitaminC),
+          const Divider(height: 5, thickness: 5),
+          const SizedBox(height: 15),
+          _buildSupplements1('Benefits:', ''),
+          _buildSupplements2('Immune System Support: ', benefits1VitaminC),
+          _buildSupplements2('Antioxidant Protection: ', benefits2VitaminC),
+          _buildSupplements2('Collagen Production:', benefits3VitaminC),
+          _buildSupplements2('Iron Absorption:', benefits4VitaminC),
+          _buildSupplements2('Heart Health:', benefits5VitaminC),
+          const Divider(height: 5, thickness: 5),
+          const SizedBox(height: 15),
+          _buildSupplements1('Sources:', sourceVitaminC),
+          const Divider(height: 5, thickness: 5),
+          const SizedBox(height: 15),
+          _buildSupplements1('Supplementation:', supplementationVitaminC),
+          const Divider(height: 5, thickness: 5),
+          const SizedBox(height: 15),
+          _buildSupplements1('Recommended Daily Intake:', intakeVitaminC),
+          const Divider(height: 5, thickness: 5),
+          const SizedBox(height: 15),
+          _buildSupplements1('Caution:', cautionVitaminC),
+        ],
+      )),
+    );
   }
+}
+
+_buildSupplements1(String title, String description) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            fontSize: 17,
+          ),
+        ),
+        Text(
+          description,
+          style: const TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 15,
+            fontStyle: FontStyle.normal,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+_buildSupplements2(String title, String description) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          description,
+          style: const TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 15,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+        const SizedBox(height: 5),
+      ],
+    ),
+  );
 }
