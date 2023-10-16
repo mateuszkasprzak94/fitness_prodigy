77318504c18353d3231141e742de020693308e65
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-10-16 20:28:31 +0200

    Add Text to Carbohydrates Page Content

--- lib/app/pages/features/diet/pages/tabs_content/nutrients/macronutrients/content/carbohydrates.dart
+++ lib/app/pages/features/diet/pages/tabs_content/nutrients/macronutrients/content/carbohydrates.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_prodigy/app/pages/features/diet/pages/tabs_content/nutrients/macronutrients/content/text_for_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -37,7 +38,62 @@ class _CarbohydratesContentState extends State<CarbohydratesContent> {
               image: AssetImage('images_diet/carbspage.jpg'),
               fit: BoxFit.cover),
         ),
+        child: ListView(
+          children: [
+            _buildCarbohydrates(
+                "Carbohydrates: The Body's Primary Energy Source",
+                carbodescription),
+            _buildCarbohydrates('Types of Carbohydrates: ',
+                'Carbohydrates can be categorized into two main types based on their chemical structure:'),
+            _buildCarbohydrates('', carbotypes),
+            _buildCarbohydrates('Functions of Carbohydrates: ',
+                'Carbohydrates are essential for various bodily functions:'),
+            _buildCarbohydrates('', carbofunction),
+            _buildCarbohydrates('Impact on Health: ',
+                'The type and quantity of carbohydrates consumed have a significant impact on health:'),
+            _buildCarbohydrates('', carboimpact),
+            _buildCarbohydrates('Dietary Sources: ',
+                'Carbohydrates can be found in a wide range of foods:'),
+            _buildCarbohydrates('', carbosource),
+            _buildCarbohydrates('Balanced Carbohydrate Intake: ', carbointake),
+            _buildCarbohydrates('Conclusion: ', carboconclusion),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget _buildCarbohydrates(String title, String description) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          color: const Color.fromARGB(160, 216, 179, 79).withOpacity(0.75),
+          child: Text(
+            title,
+            style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 20),
+          ),
+        ),
+        Container(
+          color: Colors.white.withOpacity(0.70),
+          child: Text(
+            description,
+            style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.normal,
+                fontSize: 15),
+          ),
+        ),
+      ],
+    ),
+  );
 }
--- lib/app/pages/features/diet/pages/tabs_content/nutrients/macronutrients/content/text_for_content.dart
+++ lib/app/pages/features/diet/pages/tabs_content/nutrients/macronutrients/content/text_for_content.dart
@@ -47,3 +47,39 @@ The recommended daily intake of protein can vary depending on factors like age, 
 It's important to note that a well-balanced diet that includes a variety of protein sources will help ensure that you obtain all essential amino acids.
 Including high-quality protein in your diet is essential for overall health, so be mindful of your protein intake to support your body's functions and wellbeing.
 """;
+
+// CARBOHYDRATES
+
+String carbodescription =
+    "Carbohydrates, often referred to as carbs, are one of the three essential macronutrients, along with proteins and fats. They serve as the body's primary source of energy and play a crucial role in various physiological functions. Let's explore the different types of carbohydrates, their functions, dietary sources, and their impact on health.";
+
+String carbotypes =
+    """1.	Simple Carbohydrates: These carbohydrates are composed of one or two sugar units and are quickly digested. They provide rapid but short-lived energy. Common sources include table sugar (sucrose), fruit sugar (fructose), and milk sugar (lactose).
+2.	Complex Carbohydrates: Complex carbohydrates consist of long chains of sugar units. They are found in foods like grains, legumes, vegetables, and whole fruits. Complex carbs provide sustained and more stable energy levels due to their slower digestion and absorption.
+""";
+
+String carbofunction =
+    """1.	Energy Production: Carbohydrates are the primary energy source for the body. They are converted into glucose, which is used for energy or stored in the form of glycogen in the liver and muscles for future use.
+2.	Brain Function: Glucose, derived from carbohydrates, is the primary fuel for the brain and central nervous system. It supports cognitive functions and mental clarity.
+3.	Metabolic Regulation: Carbohydrates play a role in regulating metabolism, preventing the breakdown of protein for energy and aiding in fat metabolism.
+4.	Digestive Health: Dietary fiber, a type of carbohydrate, promotes healthy digestion, regular bowel movements, and can reduce the risk of digestive issues.
+""";
+
+String carboimpact =
+    """1.	Blood Sugar Control: Simple carbohydrates, particularly refined sugars, can lead to rapid spikes and crashes in blood sugar levels. Complex carbohydrates, on the other hand, provide more stable and sustained energy.
+2.	Weight Management: Balancing calorie intake from carbohydrates with overall energy expenditure is essential for weight management. High-fiber complex carbs can promote a feeling of fullness and prevent overeating.
+3.	Heart Health: A diet rich in fiber from complex carbohydrates can help reduce cholesterol levels and lower the risk of heart disease.
+""";
+
+String carbosource =
+    """•	Grains: Foods like bread, pasta, rice, and cereal are significant sources of carbohydrates.
+•	Legumes: Beans, lentils, and peas are rich in both complex carbohydrates and protein.
+•	Vegetables: Starchy vegetables like potatoes and corn, as well as non-starchy vegetables like broccoli and spinach, contain carbohydrates.
+•	Fruits: Whole fruits, such as apples, bananas, and oranges, are natural sources of carbohydrates and fiber.
+""";
+
+String carbointake =
+    "A balanced diet includes a mix of carbohydrates from various sources, with an emphasis on complex carbohydrates and dietary fiber. It's important to limit the intake of added sugars and highly processed carbohydrates.";
+
+String carboconclusion =
+    "Carbohydrates are a vital component of a healthy diet, serving as the body's primary energy source. It's essential to make informed choices about the types and amounts of carbohydrates consumed to support overall health and well-being. Prioritizing complex carbohydrates and dietary fiber can help maintain energy levels, promote digestive health, and reduce the risk of chronic diseases.";
