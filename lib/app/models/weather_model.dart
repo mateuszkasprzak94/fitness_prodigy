47b6203bf9dc47cc7b206f6d44b1158ee9b7cf6c
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2023-10-18 13:34:24 +0200

    Add lottie to pubspec

--- lib/app/models/weather_model.dart
+++ lib/app/models/weather_model.dart
@@ -2,11 +2,14 @@ class WeatherModel {
   const WeatherModel({
     required this.city,
     required this.temperature,
+    required this.condition,
   });
   final String city;
   final double temperature;
+  final String condition;
 
   WeatherModel.fromJson(Map<String, dynamic> json)
       : city = json['location']['name'],
-        temperature = json['current']['temp_c'] + 0.0;
+        temperature = json['current']['temp_c'] + 0.0,
+        condition = json['current']['condition']['text'];
 }
