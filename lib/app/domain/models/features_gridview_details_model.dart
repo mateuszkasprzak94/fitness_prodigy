e8b752bf6a17aa256a56d538652c555f1edf8046
Mateusz Kasprzak <mateusz.kasprzak17@gmail.com>
2024-05-24 11:34:31 +0200

    Correct route

--- lib/app/domain/models/features_gridview_details_model.dart
+++ lib/app/domain/models/features_gridview_details_model.dart
@@ -2,7 +2,7 @@ import 'package:fitness_prodigy/app/core/constants.dart';
 import 'package:fitness_prodigy/app/pages/features/diet/pages/diet_page.dart';
 import 'package:fitness_prodigy/app/pages/features/event_planner/event_planner_page/page/event_planner_page.dart';
 import 'package:fitness_prodigy/app/pages/features/goals/goals_page.dart';
-import 'package:fitness_prodigy/app/pages/features/motivation_quotes/pages/motivation_quotes_page.dart';
+import 'package:fitness_prodigy/app/pages/features/motivation_quotes/pages/main_quote_page.dart';
 import 'package:fitness_prodigy/app/pages/features/supplements/supplements_page.dart';
 import 'package:fitness_prodigy/app/pages/features/water_glass_counter/water_glass_counter_page.dart';
 import 'package:flutter/material.dart';
@@ -71,7 +71,7 @@ final List<FeaturesGridViewDetails> featuresGridViewDetails = [
     onPressed: (context) {
       Navigator.of(context).push(
         MaterialPageRoute(
-          builder: (_) => const MotivationQuotesPage(),
+          builder: (_) => const MainQuotePage(),
         ),
       );
     },
